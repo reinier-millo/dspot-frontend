2238e528e56f5ff1d9f345fa4ddcef2d844920fb
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2024-09-10 03:57:02 -0500

    Load all friends on app load
    - Allow to show the correct info on the detail view

--- src/ui/components/app/App.tsx
+++ src/ui/components/app/App.tsx
@@ -1,22 +1,43 @@
 /**
  * App component to wrap the application.
  */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from '../../layout/Layout';
 import FriendsList from '../../../views/friends/FriendsList';
 import FriendDetails from '../../../views/friendDetails/FriendDetails';
+import { getFriends } from '../../../lib/api/friends';
+import { setFriends } from '../../../lib/store/friendsSlice';
 
 const App: React.FC = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const dispatch = useDispatch();
+
+  /* Fetch friends from the API when the component is mounted. 
+  This is used to populate the friends list when loading details page directly. */
+  useEffect(() => {
+    const fetchFriends = async () => {
+      const data = await getFriends();
+      dispatch(setFriends(data));
+      setIsLoading(false);
+    };
+    fetchFriends();
+  }, [dispatch]);
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<FriendsList />} />
-          <Route path="/:id/details" element={<FriendDetails />} />
-        </Route>
-      </Routes>
-    </Router>
+    <>
+      {isLoading ? <></> :
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<FriendsList />} />
+              <Route path="/:id/details" element={<FriendDetails />} />
+            </Route>
+          </Routes>
+        </Router>
+      }
+    </>
   );
 };
 
