ad8068e709dae6d64c3193ae0d0c1701e3acbeb3
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2024-09-10 03:57:41 -0500

    Add loading spinner component

---
+++ src/ui/components/loadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,14 @@
+/**
+ * Loading spinner component
+ */
+const LoadingSpinnerComponent = () => {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="w-20 h-20 border-dashed rounded-full animate-spin">
+        <img src="/logo192.png" alt="loading" />
+      </div>
+    </div>
+  )
+}
+
+export default LoadingSpinnerComponent;
