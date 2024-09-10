7b2554e81673e5974e6184e60a538bbe5c07e58f
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2024-09-10 03:58:28 -0500

    Add tabs component

---
+++ src/ui/components/tabs/tabs.tsx
@@ -0,0 +1,29 @@
+/**
+ * Tabs component
+ */
+import "../../../styles/components/tabs.scss";
+import { useState } from "react";
+
+const TabsComponent = ({ tabs, childrens, className }: { tabs: string[], childrens: React.ReactNode[], className?: string }) => {
+  const [selectedTab, setSelectedTab] = useState(0);
+  return (
+    <div className={`flex flex-col ${className}`}>
+      <div className="flex flex-row gap-2">
+        {tabs.map((tab, index) => (
+          <button
+            key={index}
+            className={`h-8 w-[65px] flex justify-center tab ${selectedTab === index ? 'tab__active' : ''}`}
+            onClick={() => setSelectedTab(index)}
+          >
+            {tab}
+          </button>
+        ))}
+      </div>
+      <div className="tab-content grow p-7">
+        {childrens[selectedTab]}
+      </div>
+    </div>
+  )
+}
+
+export default TabsComponent;
